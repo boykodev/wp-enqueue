b1b640fefaed3349b57dc421c934389c1a7ad370
Serge Boyko <boykodev@gmail.com>
2016-02-04 17:59:52 +0200

    Minor additions
    Renamed 'wp-enqueue' prefix to 'wpenq'

--- menu-template.php
+++ menu-template.php
@@ -1,8 +1,8 @@
 <div class="wrap">
     <h2>WP Enqueue Options</h2>
-
-    <form method="post" action="">
-        <?php settings_fields('wp-enqueue-settings-group'); ?>
-        <?php do_settings_sections('wp-enqueue-menu') ?>
+    <form method="post" action="options.php">
+        <?php settings_fields('wpenq-settings-group'); ?>
+        <?php do_settings_sections('wpenq-menu') ?>
+        <?php submit_button() ?>
     </form>
 </div>
--- wp-enqueue.php
+++ wp-enqueue.php
@@ -8,34 +8,43 @@ Author URI: http://www.boykodev.com
 */
 
 // create options page menu
-function wp_enqueue_create_menu() {
+function wpenq_create_menu() {
     // generate wp-enqueue menu
-    add_options_page('WP Enqueue', 'WP Enqueue', 'manage_options', 'wp-enqueue-menu', 'wp_enqueue_menu_content');
+    add_options_page('WP Enqueue', 'WP Enqueue', 'manage_options', 'wpenq-menu', 'wpenq_menu_content');
 
     // activate custom settings
-    add_action('admin_init', 'wp_enqueue_custom_settings');
+    add_action('admin_init', 'wpenq_custom_settings');
 }
 
-add_action('admin_menu', 'wp_enqueue_create_menu');
+add_action('admin_menu', 'wpenq_create_menu');
 
-function wp_enqueue_custom_settings() {
-    register_setting('wp-enqueue-settings-group', 'name');
-    add_settings_section('wp-enqueue-scripts', 'WP Enqueue Scripts', 'wp_enqueue_scripts_content', 'wp-enqueue-menu');
-    add_settings_section('wp-enqueue-styles', 'WP Enqueue Styles', 'wp_enqueue_styles_content', 'wp-enqueue-menu');
+function wpenq_custom_settings() {
+    register_setting('wpenq-settings-group', 'wpenq_title');
+    // scripts section
+    add_settings_section('wpenq-scripts', 'WP Enqueue Scripts', 'wpenq_scripts_content', 'wpenq-menu');
+    add_settings_field('wpenq-scripts-title', 'Script Title', 'wpenq_scripts_title_content', 'wpenq-menu', 'wpenq-scripts');
+    // styles section TODO
+    //add_settings_section('wpenq-styles', 'WP Enqueue Styles', 'wpenq_styles_content', 'wpenq-menu');
 }
 
 /* callback functions with content */
-function wp_enqueue_menu_content() {
+function wpenq_scripts_title_content() {
+    $title = esc_attr(get_option('wpenq_title'));
+    echo '<input type="text" name="wpenq_title" value="'.$title.'">';
+}
+
+function wpenq_menu_content() {
     require_once('menu-template.php');
 }
 
-function wp_enqueue_scripts_content() {
-    echo 'scripts';
+function wpenq_scripts_content() {
+    echo '<p>Add custom scripts here:</p>';
 }
 
-function wp_enqueue_styles_content() {
+/* TODO
+function wpenq_styles_content() {
     echo 'styles';
-}
+}*/
 
 // load scripts test
 function load_scripts() {
